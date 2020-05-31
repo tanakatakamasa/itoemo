f2671b5933e1b6579ec0c12d3901a6ca324e934c
tanakatakamasa <62592199+tanakatakamasa@users.noreply.github.com>
2020-05-31 16:15:09 +0900

    Update 20200525081300_devise_create_users.rb

--- db/migrate/20200525081300_devise_create_users.rb
+++ db/migrate/20200525081300_devise_create_users.rb
@@ -33,11 +33,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.datetime :locked_at
 
 
-      t.timestamps : false
+      t.timestamps :false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    add_index :users,:email,                unique: true
+    add_index :users,:reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
